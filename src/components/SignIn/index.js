a0ea08b2695b6a59ed73baf10f074b279343197d
edenAbdisa <eden.abdisa@yahoo.com>
2021-09-13 14:59:46 +0300

    loginfix

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -60,24 +60,24 @@ class SignIn extends Component {
           password:password
         })
       }).then((response) => {
-          var res=response.data.data;
+          var res=response.data;
           // check if the data is populated
           this.setState({ loadingData: false});
-          if(response.data.contents[0].success){
+          if(response.contents[0].success){
             localStorage.setItem('auth', true);      
             localStorage.setItem('role', res.type.toString());
             localStorage.setItem('token', res.remember_token.toString());
             this.props.history.push(ROUTES.REPORT); 
           } else{
-            this.setState({error:response.data.contents[0].error});
+            this.setState({error:response.contents[0].error});
           } 
           if (this.state.loadingData) {
             this.login();
           }
       }).catch((e) => {        
-        var err=e.response.data.content[0].error;
+        var err=e.response.content[0].error;
         if(e.response.status === 400){
-          var err=e.response.data.content[0].error;
+          var err=e.response.content[0].error;
           this.state.error=(err.email?JSON.stringify(err.email):"")
           +'\n'+ (err.password?JSON.stringify(err.password):"");  
         }else{
